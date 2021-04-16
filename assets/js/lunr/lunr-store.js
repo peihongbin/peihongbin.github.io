674b348e0ea24713c0aecce8655e7930c6d83ac2
peihongbin <peihongbin@hotmail.com>
2021-04-16 12:54:37 +0800

    update

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,19 @@
 var store = [{
         "title": "golang runtime",
-        "excerpt":" ","categories": ["golang"],
+        "excerpt":"golang runtime   goroutine  go  ","categories": ["golang"],
         "tags": [],
         "url": "/golang/2021/04/03/golang-runtime.html",
+        "teaser": null
+      },{
+        "title": "kubernetes service",
+        "excerpt":"kubernetes service   kubernetes  ","categories": ["kubernetes"],
+        "tags": [],
+        "url": "/kubernetes/2021/04/11/kubernetes-service.html",
+        "teaser": null
+      },{
+        "title": "python test",
+        "excerpt":"python test   broken unit test  ","categories": ["test"],
+        "tags": [],
+        "url": "/test/2021/04/11/python-test.html",
         "teaser": null
       }]
